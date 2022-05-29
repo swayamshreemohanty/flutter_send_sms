7b64aa0d03f628b7622b03de646c8835a7b0e093
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-05-29 07:58:03 +0530

    device orientation fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:send_sms/sms_send_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
@@ -20,4 +26,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
